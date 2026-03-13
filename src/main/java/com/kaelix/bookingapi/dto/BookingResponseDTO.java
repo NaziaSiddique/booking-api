a2a44be7db7a95a559c47a55557974a2d3868974
NaziaSiddique <nazia.siddique04@gmail.com>
2026-03-13 21:45:57 +0000

    Add BookingResponseDTO

---
+++ src/main/java/com/kaelix/bookingapi/dto/BookingResponseDTO.java
@@ -0,0 +1,61 @@
+package com.kaelix.bookingapi.dto;
+
+import java.time.LocalDate;
+
+public class BookingResponseDTO {
+
+    private Long id;
+    private String guestName;
+    private LocalDate checkInDate;
+    private LocalDate checkOutDate;
+    private Double price;
+    private String status;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getGuestName() {
+        return guestName;
+    }
+
+    public void setGuestName(String guestName) {
+        this.guestName = guestName;
+    }
+
+    public LocalDate getCheckInDate() {
+        return checkInDate;
+    }
+
+    public void setCheckInDate(LocalDate checkInDate) {
+        this.checkInDate = checkInDate;
+    }
+
+    public LocalDate getCheckOutDate() {
+        return checkOutDate;
+    }
+
+    public void setCheckOutDate(LocalDate checkOutDate) {
+        this.checkOutDate = checkOutDate;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
