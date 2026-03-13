943959f313ed7d257513c1421438265142fde7f3
NaziaSiddique <nazia.siddique04@gmail.com>
2026-03-13 21:43:38 +0000

    Add getters and setters to BookingEntity

--- src/main/java/com/kaelix/bookingapi/entity/BookingEntity.java
+++ src/main/java/com/kaelix/bookingapi/entity/BookingEntity.java
@@ -20,4 +20,47 @@ public class BookingEntity {
 
     private String status;
 
+    public Long getId() {
+        return id;
+    }
+
+    public String getGuestName() {
+        return guestName;
+    }
+
+    public void setGuestName(String guestName) {
+        this.guestName = guestName;
+    }
+
+    public LocalDate getCheckInDate() {
+        return checkInDate;
+    }
+
+    public void setCheckInDate(LocalDate checkInDate) {
+        this.checkInDate = checkInDate;
+    }
+
+    public LocalDate getCheckOutDate() {
+        return checkOutDate;
+    }
+
+    public void setCheckOutDate(LocalDate checkOutDate) {
+        this.checkOutDate = checkOutDate;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
