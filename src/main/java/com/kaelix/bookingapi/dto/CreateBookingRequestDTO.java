f01bd1371679d64a72edcab18f0b3d39d046b687
NaziaSiddique <nazia.siddique04@gmail.com>
2026-03-13 21:45:11 +0000

    Add CreateBookingRequestDTO

---
+++ src/main/java/com/kaelix/bookingapi/dto/CreateBookingRequestDTO.java
@@ -0,0 +1,36 @@
+package com.kaelix.bookingapi.dto;
+
+import java.time.LocalDate;
+
+public class CreateBookingRequestDTO {
+
+    private String guestName;
+
+    private LocalDate checkInDate;
+
+    private LocalDate checkOutDate;
+
+    public String getGuestName() {
+        return guestName;
+    }
+
+    public void setGuestName(String guestName) {
+        this.guestName = guestName;
+    }
+
+    public LocalDate getCheckInDate() {
+        return checkInDate;
+    }
+
+    public void setCheckInDate(LocalDate checkInDate) {
+        this.checkInDate = checkInDate;
+    }
+
+    public LocalDate getCheckOutDate() {
+        return checkOutDate;
+    }
+
+    public void setCheckOutDate(LocalDate checkOutDate) {
+        this.checkOutDate = checkOutDate;
+    }
+}
